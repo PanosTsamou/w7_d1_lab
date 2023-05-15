2b63cc7a0932a27817e421760dad24c76a117478
Daniel FCS <danielf.cs@outlook.com>
2023-05-15 16:31:33 +0100

    first commit

---
+++ lab_events_start/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector("#new-item-form")
+  form.addEventListener("submit",handleForm)
+
+  const deleteButton = document.querySelector("#delete-button")
+  deleteButton.addEventListener("click",handleDelete)
+
+})
+
+
+const handleForm = function(event) {
+  event.preventDefault()
+
+  // const holdMyThings = []
+  // const book = {}
+
+  const formTitle = event.target.title.value
+  const formAuthor = event.target.author.value
+  const formCategory = event.target.category.value
+
+  const readList = document.querySelector('#reading-list')
+  const newListItem = document.createElement("li")
+
+  newListItem.classList.add("book")
+
+  const paraTitle = document.createElement("p")
+  const paraAuthor = document.createElement("p")
+  const paraCategory = document.createElement("p")
+  
+  paraTitle.textContent = `Title: ${formTitle}`
+  paraAuthor.textContent = `Author: ${formAuthor}`
+  paraCategory.textContent = `Category: ${formCategory}`
+
+  newListItem.appendChild(paraTitle)
+  newListItem.appendChild(paraAuthor)
+  newListItem.appendChild(paraCategory)
+
+  readList.appendChild(newListItem)
+  // console.log(my_list[0])
+
+  console.log(formTitle,formAuthor,formCategory)
+  event.target.reset() 
+
+}
+
+const handleDeleteAllClick = function (event) {
+  const readingList = document.querySelector('#reading-list');
+  readingList.innerHTML = '';}
